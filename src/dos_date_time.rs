e7c3306806c6f03cb0ac191916d6db9f885dbfbd
Shun Sakai <sorairolake@protonmail.ch>
2026-09-09 09:02:24 +0900

    Fix formatting of documentation comment in dos_date_time.rs

--- src/dos_date_time.rs
+++ src/dos_date_time.rs
@@ -68,8 +68,8 @@ impl DateTime {
     ///
     /// <div class="warning">
     ///
-    /// This function may round towards zero, truncating more precise times that a
-    /// `DateTime` cannot store.
+    /// This function may round towards zero, truncating more precise times that
+    /// a `DateTime` cannot store.
     ///
     /// </div>
     ///
